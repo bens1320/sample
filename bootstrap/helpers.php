b622e8e6f3464ba1f262e8358c79e568592795a6
bens1320 <bens1320@qq.com>
2017-08-20 15:48:25 +0000

    Updated database configuration0

---
+++ bootstrap/helpers.php
@@ -0,0 +1,18 @@
+<?php
+
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
+            'connetion' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    } else {
+
+    }
+}
